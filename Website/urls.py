c63b5cd677e53b335745a0ee401a91e5230101e4
Matthew Doyle <mattdoyle@Matthews-MacBook-Air.local>
2019-02-08 21:57:39 +0000

    Removing LPC from address

--- Website/urls.py
+++ Website/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-        path('LPC/', include('LPC.urls')),
+        path('', include('LPC.urls')),
         path('admin/', admin.site.urls)
         ]
