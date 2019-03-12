67a25ff23df96302a8dbe6074174f2affd57be6d
Matthew Doyle <mattdoyle@Matthews-MacBook-Air.local>
2019-03-12 00:59:34 +0000

    More changes

--- LPC/admin.py
+++ LPC/admin.py
@@ -7,7 +7,6 @@ class ParentAdmin(admin.ModelAdmin):
     list_disply_links = ('punter',)
     list_editable = ('bet_date','bet_week','bet_number','bet_legs','bet_event','bet_amount','bet_odds','bet_return','bet_win','bet_cashed')
     list_filter = ('punter','bet_date','bet_week','bet_number','bet_legs','bet_event','bet_amount','bet_odds','bet_return','bet_win','bet_cashed')
-    search_fields = ('punter','bet_date','bet_week','bet_number','bet_legs','bet_event','bet_amount','bet_odds','bet_return','bet_win','bet_cashed')
     ordering = ['-bet_date','punter','bet_number']
     
 @admin.register(Multi)
@@ -16,5 +15,4 @@ class MultiAdmin(admin.ModelAdmin):
     list_display_links = ('parent',)
     list_editable = ('bet_leg','bet_type','bet_team','bet_opposition','bet_home','bet_odds','bet_win')
     list_filter = ('parent','bet_leg','bet_type','bet_team','bet_opposition','bet_home','bet_odds','bet_win')
-    search_fields = ('parent','bet_leg','bet_type','bet_team','bet_opposition','bet_home','bet_odds','bet_win')
     ordering = ['-parent','bet_leg']
